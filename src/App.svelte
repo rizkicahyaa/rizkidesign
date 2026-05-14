a5eb4b80cb0ed6ebf520b1bacaeafafb4be613e1
Rizki Cahya <rizki.cahya03@gmail.com>
2026-05-14 21:37:56 +0700

    feat: change font & add navbar components

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,10 @@
+<script>
+	import Navbar from './components/Navbar.svelte';
+</script>
+
 <svelte:head>
+	<link rel="stylesheet" href="app.css" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
 </svelte:head>
+
+<Navbar />
