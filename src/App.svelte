38123ece9b28b956620e94226b5cd10928550bc7
Rizki Cahya <rizki.cahya03@gmail.com>
2026-05-14 22:29:27 +0700

    refactor: navbar components

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <svelte:head>
-	<link rel="stylesheet" href="app.css" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
 </svelte:head>
 
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,32 +1,27 @@
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+<nav class="navbar navbar-expand-sm navbar-dark shadow-sm py-3">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="/">Expand at sm</a>
+		<a class="navbar-brand" href="/">rizkidesign.id</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
 		<div class="collapse navbar-collapse" id="navbarsExample03">
-			<ul class="navbar-nav me-auto mb-2 mb-sm-0">
+			<ul class="navbar-nav m-auto mb-2 mb-sm-0">
 				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="/">Home</a>
+					<a class="nav-link" href="/">Home</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="/">Link</a>
+					<a class="nav-link" href="/">About</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
+					<a class="nav-link" href="/">Portfolio</a>
 				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="/" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-					<ul class="dropdown-menu" aria-labelledby="dropdown03">
-						<li><a class="dropdown-item" href="/">Action</a></li>
-						<li><a class="dropdown-item" href="/">Another action</a></li>
-						<li><a class="dropdown-item" href="/">Something else here</a></li>
-					</ul>
+				<li class="nav-item">
+					<a class="nav-link" href="/">Contact</a>
 				</li>
 			</ul>
 			<form>
-				<input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+				<input class="form-control search" type="text" placeholder="Search" aria-label="Search" />
 			</form>
 		</div>
 	</div>
