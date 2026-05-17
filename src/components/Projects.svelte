7fda052b0276e3d6c1baf2a279c70a7f1bd6736f
Rizki Cahya <rizki.cahya03@gmail.com>
2026-05-17 14:57:25 +0700

    feat: setup aos

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,7 +1,21 @@
 <script>
+	import AOS from 'aos';
+	import 'aos/dist/aos.css';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		AOS.init({
+			duration: 1000,
+			once: true
+		});
+	});
+
 	import GaviPoster from '../assets/poster-barca-gavi.png';
 	import MessiPoster from '../assets/poster-messi.png';
 	import BrunoPoster from '../assets/poster-mu-bruno.png';
+	import GarnaPoster from '../assets/poster-mu-garnacho-remake.png';
+	import PalmerPoster from '../assets/poster-chelsea-palmer.png';
+	import MartinezPoster from '../assets/poster-inter-martinez.png';
 </script>
 
 <section class="projects py-5">
@@ -12,14 +26,47 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-4">
-				<img src={GaviPoster} alt="" />
+			<div class="col-md-4 g-2">
+				<div class="card" data-aos="fade-up">
+					<div class="card-body">
+						<img src={GaviPoster} alt="" />
+					</div>
+				</div>
 			</div>
-			<div class="col-md-4">
-				<img src={MessiPoster} alt="" />
+			<div class="col-md-4 g-2">
+				<div class="card">
+					<div class="card-body">
+						<img src={MessiPoster} alt="" />
+					</div>
+				</div>
 			</div>
-			<div class="col-md-4">
-				<img src={BrunoPoster} alt="" />
+			<div class="col-md-4 g-2">
+				<div class="card">
+					<div class="card-body">
+						<img src={BrunoPoster} alt="" />
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 g-2">
+				<div class="card">
+					<div class="card-body">
+						<img src={GarnaPoster} alt="" />
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 g-2">
+				<div class="card">
+					<div class="card-body">
+						<img src={PalmerPoster} alt="" />
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 g-2">
+				<div class="card">
+					<div class="card-body">
+						<img src={MartinezPoster} alt="" />
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
