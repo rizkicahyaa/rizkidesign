edec74268876f2089c008ab67ff2cf1af750e647
Rizki Cahya <rizki.cahya03@gmail.com>
2026-05-21 01:05:50 +0700

    feat: added social media contact in contact section

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -7,7 +7,7 @@
 		</div>
 		<div class="row">
 			<div class="col-md-6 py-3 px-3">
-				<div class="card px-5 py-5" style="background-color: aliceblue;" data-aos="zoom-in">
+				<div class="card px-5 py-5 h-100" style="background-color: aliceblue;" data-aos="zoom-in">
 					<h5 class="text-center mb-4">GET IN TOUCH</h5>
 					<div class="d-flex align-items-start">
 						<i class="bi bi-envelope me-3"></i>
@@ -26,20 +26,27 @@
 				</div>
 			</div>
 			<div class="col-md-6 py-3 px-3">
-				<div class="card py-5 px-5" style="background-color: #FAF0E6;" data-aos="zoom-in" data-aos-delay="500">
+				<div class="card py-5 px-5 h-100" style="background-color: #FAF0E6;" data-aos="zoom-in" data-aos-delay="500">
 					<h5 class="text-center mb-4">FOLLOW ME</h5>
 					<div class="d-flex align-items-start">
-						<i class="bi bi-envelope me-3"></i>
+						<i class="bi bi-instagram me-3"></i>
 						<div>
-							<h5 class="mb-0">Email</h5>
-							<p>rizki.cahya03@gmail.com</p>
+							<h5 class="mb-0">Instagram</h5>
+							<a href="https://instagram.com/millioree" class="text-black" target="_blank"><p>@millioree</p></a>
 						</div>
 					</div>
 					<div class="d-flex align-items-start">
-						<i class="bi bi-telephone me-3"></i>
+						<i class="bi bi-facebook me-3"></i>
 						<div>
-							<h5 class="mb-0">Phone</h5>
-							<p>(+62) 878 6600 5508</p>
+							<h5 class="mb-0">Facebook</h5>
+							<a href="https://web.facebook.com/rizkicahyaa" class="text-black" target="_blank"><p>rizkicahyaa</p></a>
+						</div>
+					</div>
+					<div class="d-flex align-items-start">
+						<i class="bi bi-linkedin me-3"></i>
+						<div>
+							<h5 class="mb-0">LinkedIn</h5>
+							<a href="https://linkedin.com/in/rizkicahya" class="text-black" target="_blank"><p>rizkicahya</p></a>
 						</div>
 					</div>
 				</div>
